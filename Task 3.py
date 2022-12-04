8d3c9d2b6b1b9cb8d647667664e80e6d9d10563d
EsUnoAlpha <1687462@gmail.com>
2022-12-04 18:36:41 +0300

    homework added

--- Task 3.py
+++ Task 3.py
@@ -1,8 +1,13 @@
+"""Сложность: О(n)
+Алгоритм ищет совпадения в строках и прибавляет единицу если найдено совпадение"""
+
 def jewelry(jewels, stones):
-    counter = 0
-    for i in jewels:
-        for n in stones:
+    counter = 0 #Вводится счетчик
+    for i in jewels: #Поиск в первом списке
+        for n in stones: #Поиск во втором списке
             if i == n:
                 counter += 1
     return counter
+
+print(jewelry('z', 'ZZ'))
 
